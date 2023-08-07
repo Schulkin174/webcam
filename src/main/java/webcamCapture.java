7c4f086dc751fbf4117715037f9b78a84851e922
Aleksandr Schulkin <75139215+Schulkin174@users.noreply.github.com>
2023-08-08 00:29:21 +0300

    Update webcamCapture.java

--- src/main/java/webcamCapture.java
+++ src/main/java/webcamCapture.java
@@ -5,16 +5,16 @@ import org.opencv.highgui.HighGui;
 
 public class webcamCapture {
     public static void main(String[] args) {
-        // Загрузите библиотеку OpenCV
+        // Загружаю библиотеку OpenCV
         // System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // метод не сработал: вероятно, требуется настройка пути
         nu.pattern.OpenCV.loadShared(); // аналогичный метод - сработал
 
 
 
-        // Создайте объект VideoCapture для захвата видео с веб-камеры
-        VideoCapture capture = new VideoCapture(0); // 0 - индекс вашей веб-камеры
+        // Создаю объект VideoCapture для захвата видео с веб-камеры
+        VideoCapture capture = new VideoCapture(0); // 0 - индекс веб-камеры
 
-        // Проверьте, удалось ли открыть видео поток
+        // Проверяю, удалось ли открыть видео поток
         if (!capture.isOpened()) {
             System.out.println("Ошибка при открытии веб-камеры.");
             return;
@@ -24,7 +24,7 @@ public class webcamCapture {
         while (true) {
             Mat frame = new Mat();
             if (capture.read(frame)) {
-                // Ваш код обработки кадра
+                // код обработки кадра
                 // Например, вывод кадра на экран:
                 HighGui.imshow("Webcam", frame);
                 HighGui.waitKey(10);
